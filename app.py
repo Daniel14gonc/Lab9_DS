90fb433bfdd41c496a9beed8ae1e8d013ca5c71a
Daniel <gon20293@uvg.edu.gt>
2023-10-13 21:21:29 -0600

    add: muestra de serie de tiempo

--- app.py
+++ app.py
@@ -1,24 +1,27 @@
 from dash import Dash, html, dcc, callback, Output, Input
 import plotly.express as px
 import pandas as pd
+import plotly.graph_objs as go
 
-df = pd.read_csv('https://raw.githubusercontent.com/plotly/datasets/master/gapminder_unfiltered.csv')
+# df = pd.read_csv('https://raw.githubusercontent.com/plotly/datasets/master/gapminder_unfiltered.csv')
+# print(df.head())
+df = pd.read_csv('importacion_regular.csv')
 
 app = Dash(__name__)
 
 app.layout = html.Div([
-    html.H1(children='Title of Dash App', style={'textAlign':'center'}),
-    dcc.Dropdown(df.country.unique(), 'Canada', id='dropdown-selection'),
-    dcc.Graph(id='graph-content')
+    html.H1('Gráfico de Serie de Tiempo'),
+    dcc.Graph(id='grafico-tiempo'),
 ])
 
-@callback(
-    Output('graph-content', 'figure'),
-    Input('dropdown-selection', 'value')
+# Callback para actualizar el gráfico en función de los filtros
+@app.callback(
+    Output('grafico-tiempo', 'figure'),
+    [Input('grafico-tiempo', 'relayoutData')]
 )
-def update_graph(value):
-    dff = df[df.country==value]
-    return px.line(dff, x='year', y='pop')
+def update_graph(relayoutData):
+    figura = px.line(df, x='Fecha', y='Gasolina_regular', title='Serie de Tiempo')
+    return figura
 
 if __name__ == '__main__':
     app.run(debug=True)
